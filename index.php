f65d3353971cf142af5d3ada21a40c35b3a3a3e6
marcusgreen <marcusgreen@mailbox.org>
2020-01-19 16:01:29 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *
+ * @package    local_dbdemo
+ * @copyright  2020 Marcus Green
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+global $CFG, $PAGE;
+require_once(__DIR__ . '/../../config.php');
+require_once($CFG->libdir . '/formslib.php');
+
+$PAGE->set_context(context_system::instance());
+$PAGE->set_url('/local/dbdemo.php');
+$filename = '';
+class local_dbdemo_form extends moodleform {
+    protected function definition() {
+        global $DB,$USER;
+        $mform = $this->_form;
+
+        $mform->addElement('text','demotext');
+        $mform->setType('demotext',PARAM_RAW);
+
+$mform = new local_dbdemo_form();
+echo $OUTPUT->header();
+$mform->display();
+echo $OUTPUT->footer();
+if($data =$mform->get_data()){
+
+}
